c734b4fa61ddacb070311217c3b0063b528341dc
Alexander Nesterov <alex18nest@icloud.com>
2024-08-22 15:49:27 -0300

    [+] add main

--- Exceptions_in_constructors/FigureError.cpp
+++ Exceptions_in_constructors/FigureError.cpp
@@ -5,5 +5,6 @@
 //  Created by Александр Нестеров on 19/8/24.
 //
 
+#include <string>
 #include "FigureError.hpp"
 FigureError::FigureError(const std::string& message): std::domain_error("Ошибка создания фигуры. " + message) {}
